faf44a14fbfcc1f2b2ed4a36e12e510bffbb7da8
Worawat Chueajedton <worawat.chu@ku.th>
2018-01-19 14:48:43 +0700

    Complete Coin

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -1,6 +1,9 @@
 package coinpurse;
 
 import java.lang.Comparable;
+import java.util.ArrayList;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 /**
  * Coin represents coinage (money) with a fixed value and currency.
@@ -25,13 +28,32 @@ public class Coin implements Comparable<Coin> {
 		return currency;
 	}
 
+	public boolean equal(Object arg) {
+		if (arg == null)
+			return false;
+		if (arg.getClass() != this.getClass())
+			return false;
+		Coin other = (Coin) arg;
+		if (other.getValue() == value && other.getCurrenccy().equalsIgnoreCase(currency)) {
+			return true;
+		}
+		return false;
+	}
+
 	@Override
 	public int compareTo(Coin arg0) {
-		
+		if (value < arg0.getValue()) {
+			return -1;
+		} else if (value > arg0.getValue()) {
+			return 1;
+		} else if (value == arg0.getValue()) {
+			return 0;
+		}
 		return 0;
 	}
 
 	public String toString() {
-       return value+"-"+currency;
+		return value + "-" + currency;
 	}
+	
 }
