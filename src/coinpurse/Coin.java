a265b02af0287238eab59b97f875eacef7b24d86
Worawat Chueajedton <worawat.chu@ku.th>
2018-01-21 02:31:09 +0700

    Add Coin javadoc

--- src/coinpurse/Coin.java
+++ src/coinpurse/Coin.java
@@ -11,22 +11,54 @@ import java.util.List;
  */
 
 public class Coin implements Comparable<Coin> {
+
 	private double value;
 	private String currency;
+
+	/**
+	 * 
+	 * Make a coins with value and currency.
+	 * 
+	 * @param value
+	 *            value of coin
+	 * @param currency
+	 *            currency of coin
+	 */
 
 	public Coin(double value, String currency) {
 		this.value = value;
 		this.currency = currency;
 	}
 
+	/**
+	 * 
+	 * Get a coin's value.
+	 * 
+	 * @return value of coin
+	 * 
+	 */
 	public double getValue() {
 		return value;
 	}
 
+	/**
+	 * 
+	 * Get a coin's currency
+	 * 
+	 * @return currency of coin
+	 */
 	public String getCurrenccy() {
 		return currency;
 	}
 
+	/**
+	 * 
+	 * Two coins are equal if they have the same value and same currency
+	 * 
+	 * @param arg
+	 *           An object you want to check, Is it equal?
+	 * @return if it equal return true, if not return false.
+	 */
 	public boolean equal(Object arg) {
 		if (arg == null)
 			return false;
@@ -39,6 +71,14 @@ public class Coin implements Comparable<Coin> {
 		return false;
 	}
 
+	/**
+	 * 
+	 * To compare which one is greater of less than another one.
+	 * 
+	 * @param arg0
+	 *           coin that you want to check. 
+	 * 
+	 */
 	@Override
 	public int compareTo(Coin arg0) {
 		if (value < arg0.getValue()) {
@@ -51,6 +91,13 @@ public class Coin implements Comparable<Coin> {
 		return 0;
 	}
 
+	/**
+	 * 
+	 * Show description of coin
+	 * 
+	 * @return coin's description
+	 * 
+	 */
 	public String toString() {
 		return value + "-" + currency;
 	}
